947aba527ee1c6b97e2cf5e85ff39b42ad56acb3
Leon Lubberdink <lubberdink@strato.de>
2023-09-21 15:20:35 +0200

    Focus Search bar on Enter keydown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { BackIcon } from "./Icons";
 import { StarRating } from "./StarRating";
 
@@ -160,6 +160,26 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+
+      return () => document.removeEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
+
   return (
     <input
       className="search"
@@ -167,6 +187,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
@@ -267,7 +288,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 
       document.addEventListener("keydown", callback);
 
-      return function () {
+      return () => {
         document.removeEventListener("keydown", callback);
       };
     },
